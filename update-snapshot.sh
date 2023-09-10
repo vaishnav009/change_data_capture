f973e0a34036d9ddd3701d05297f17afa9cea8c2
Vaishnav <vaishnavkolte123@gmail.com>
2023-09-10 19:22:38 +0530

    added pre-push and pre-commit githooks

---
+++ update-snapshot.sh
@@ -0,0 +1,24 @@
+echo "Updating Snapshot"
+
+if ! [[ -v AWS_ACCOUNT_ID ]]
+then
+    echo 'AWS_ACCOUNT_ID not set. Setting it to 948045320516'
+    export AWS_ACCOUNT_ID=948045320516
+fi
+if ! [[ -v AWS_REGION]]
+then
+    echo 'AWS region not set. Setting it to us-east-1'
+    export AWS_REGION = 'us-east-1'
+fi
+if ! [[ -v AWS_ENVIRONMENT]]
+then
+    echo 'AWS_ENVIRONMENT not set. Setting it to dev'
+    export AWS_ENVIRONMENT = 'dev'
+fi
+
+export AWS_ENVIRONMENT
+export AWS_REGION
+
+pushd infra/cdk
+python -m pytest tests/snapshot/ --snapshot-update
+popd
