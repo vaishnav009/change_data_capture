d9ed433420cb7421eaef1879e300570eb3d6f1f2
Vaishnav <vaishnavkolte123@gmail.com>
2023-09-11 21:22:20 +0530

    added glue and lambda script

--- src/lambdas/handlers/glue_invocator.py
+++ src/lambdas/handlers/glue_invocator.py
@@ -1,26 +1,25 @@
 import json
 import boto3
 
-
 def lambda_handler(event, context):
-    print("We'll invoke glue job from here ....!!!")
-    print(event)
-    print(event["Records"][0]['s3']['bucket']['name'])
-    print(event["Records"][0]['s3']['object']['key'])
     
-    cdc_bucket_name = event["Records"][0]['s3']['bucket']['name']
-    changed_data_file_key = event["Records"][0]['s3']['object']['key']
     
-    client = boto3.client('glue')
-    response = client.start_job_run(
-        JobName = 'cdc-glue-job',
+    bucketName = event["Records"][0]["s3"]["bucket"]["name"]
+    fileName = event["Records"][0]["s3"]["object"]["key"]
+    
+    print(bucketName, fileName)
+        
+    glue = boto3.client('glue')
+
+    response = glue.start_job_run(
+        JobName = 'glueCDC-pyspark',
         Arguments = {
-        "--s3-target-path-key": changed_data_file_key,
-        "--s3-target-bucket": cdc_bucket_name
-        }
+            '--s3_target_path_key': fileName,
+            '--s3_target_path_bucket': bucketName
+        } 
     )
-    print(response)
+    
     return {
-        'status_code': 200,
-        'body': json.dumps('Glue job invoked successfully..!!')
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
     }
--- update-snapshot.sh
+++ update-snapshot.sh
@@ -5,11 +5,13 @@ then
     echo 'AWS_ACCOUNT_ID not set. Setting it to 948045320516'
     export AWS_ACCOUNT_ID=948045320516
 fi
+
 if ! [[ -v AWS_REGION]]
 then
     echo 'AWS region not set. Setting it to us-east-1'
     export AWS_REGION = 'us-east-1'
 fi
+
 if ! [[ -v AWS_ENVIRONMENT]]
 then
     echo 'AWS_ENVIRONMENT not set. Setting it to dev'
